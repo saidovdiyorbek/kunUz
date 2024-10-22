9f5c8fa69c3975dcce707ff0f804b0ecdde490f2
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-10-22 18:25:58 +0500

    some classes update and created

--- src/main/java/dasturlash/uz/kun_uz/exp/ExceptionHandlerController.java
+++ src/main/java/dasturlash/uz/kun_uz/exp/ExceptionHandlerController.java
@@ -3,23 +3,32 @@ package dasturlash.uz.kun_uz.exp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.*;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
 
-
-    Map<String, Object> body = new LinkedHashMap<>();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+            @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, Object>> handValidationException(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
-        body.put("status", status.value());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
 
-    List<String> errors = new LinkedList<>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-        errors.add(error.getDefaultMessage());
-    }
+        List<String> errors = new LinkedList<>();
+        for (FieldError error1 : ex.getBindingResult().getFieldErrors()){
+            errors.add(error1.getDefaultMessage());
+        }
         body.put("errors", errors);
-        return new ResponseEntity<>(body, headers, status);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler(AppBadException.class)
     public ResponseEntity<String> handleBadException(AppBadException ex) {
